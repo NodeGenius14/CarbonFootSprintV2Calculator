19bf3b74a3c21609169ab07f5fd38646520e4a4b
Arthur Lecomte <arthurlecomtefr@gmail.com>
2023-07-04 21:54:09 +0200

    Ajout Bing Map

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+/*@author Arthur Lecomte <arthurlecomtefr@gmail.com> 
+          & Baptiste Dudonné <bapt.14@hotmail.com> */ 
+
 const express = require('express');
 const fs	  = require('fs')  	  ;
 const port = process.env.PORT || 3000;
@@ -5,7 +8,7 @@ const port = process.env.PORT || 3000;
 const lang = process.env.LANG.slice(0, 2);
 const app = express();
 
-app.get('/', (req, res) =>
+app.get('/', (__, res) =>
 {
 	let indexHTML = fs.readFileSync(__dirname + '/public/html/en/index.html', 'utf-8');
 
@@ -16,6 +19,17 @@ app.get('/', (req, res) =>
 	  res.send(indexHTML);
 });
 app.use(express.static(__dirname + '/public/css'));
+app.use(express.static(__dirname + '/src'));
+
+app.get("/*", (__, res) =>
+{
+	let HTML = fs.readFileSync(__dirname + "/public/html/en/404.html", 'utf-8');
+	  if (lang ==="fr")
+	  {
+		  HTML = fs.readFileSync(__dirname + "/public/html/fr/404.html", 'utf-8');
+	  }
+	res.send(HTML);
+});
 
 app.listen(port, () => 
 {

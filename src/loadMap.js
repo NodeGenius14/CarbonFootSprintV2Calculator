3ec6b788daf6c43cf4c1e88f9c607a193600e721
Baltazar <baptiste.dudonne@etu.uca.fr>
2023-09-04 17:20:40 +0200

    désactivation controles map, modification taille buttons travelMode

--- src/loadMap.js
+++ src/loadMap.js
@@ -6,6 +6,7 @@
 
 // Obligé de mettre cette fonction en dehors de la classe car sinon il y a un problème de callback
 
+/*
 function loadMap()
 {
 	console.log("loadMap");
@@ -15,7 +16,11 @@ function loadMap()
 				center: new Microsoft.Maps.Location(48.86666,  2.333333),
 				zoom: 5,
 				liteMode: true,
-		
+				mapTypeId: Microsoft.Maps.MapTypeId.road, // Type de carte (route, aérien, etc.)
+				disableZooming: true, // Désactiver le zoom
+				disablePanning: true, // Désactiver le défilement
+				showDashboard: false
+					
 	};
 	Microsoft.Maps.loadModule('Microsoft.Maps.AutoSuggest', function () 
 	{
@@ -32,4 +37,28 @@ function loadMap()
 	
 
 			
+}
+*/
+function loadMap() {
+    console.log("loadMap");
+    map = new Microsoft.Maps.Map('#myMap', {
+        center: new Microsoft.Maps.Location(48.86666, 2.333333),
+        zoom: 5,
+        liteMode: true,
+        mapTypeId: Microsoft.Maps.MapTypeId.road, // Type de carte (route, aérien, etc.)
+        /*disableZooming: true, // Désactiver le zoom
+        disablePanning: true, */// Désactiver le défilement
+        showZoomButtons: true,
+		showDashboard: false
+		});
+
+    Microsoft.Maps.loadModule('Microsoft.Maps.AutoSuggest', function () {
+        optionAutoSuggestions = {
+            maxResults: 4,
+            map: map
+        }
+    });
+
+    ctrl = new Controleur(map);
+    ctrl.getVue.init();
 }
